4ae4a1bcd9c8636c06813de6230ad9dd0b7a1f53
Valentin-Marko <69463262+Valentin-Marko@users.noreply.github.com>
2020-10-12 17:20:38 +0300

    tema3

---
+++ Cîmpan-Valentin-Marko/L03/Program.cs
@@ -0,0 +1,57 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace L03
+{
+    class Program
+    {
+
+        static string[] Scopes = { DriveService.Scope.Drive };
+        static string ApplicationName = "DATC";
+        static DriveService service;
+        static void Main(string[] args)
+        {
+            Initialize();
+        }
+
+        static void Initialize()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("DATC.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    Environment.UserName,
+                    CancellationToken.None,
+
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Drive API service.
+            service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        static void GetAllFiles()
+        {
+
+        }
+
+    }
+}
